1540efcf84ecc6c100c0f41f871fcd37fa821e35
Harish <harish.rajkumar2022@vitstudent.ac.in>
2022-12-17 20:51:52 +0530

    Update solution for VPROPEL POD 17-12-22

--- VPROPEL/POD/2022/December/17-12-22/main.py
+++ VPROPEL/POD/2022/December/17-12-22/main.py
@@ -1,13 +1,9 @@
 input()
 l = tuple(map(int, input().split()))
-m = min(l)
 
-if m == 1:
-    print("NO")
+for i in range(2, min(l)+1):
+    if not any(tuple(n%i for n in l)):
+        print("YES")
+        break
 else:
-    for i in range(2, m+1):
-        if not any(tuple(n%i for n in l)):
-            print("YES")
-            break
-    else:
-        print("NO")
+    print("NO")
