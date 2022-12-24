74fb9f8373f645d96f031f40c9c1c588e6b62ae8
Harish <harish.rajkumar2022@vitstudent.ac.in>
2022-12-24 06:20:56 +0530

    Add solution for LeetCode Problem 242. Contains Duplicate

---
+++ LeetCode/Arrays&Hashing/242.contains-duplicate.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/contains-duplicate/
+# Time: O(n)
+# Space: O(n)
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashset = set()
+        
+        for n in nums:
+            if n in hashset:
+                return True
+            else:
+                hashset.add(n)
+        return False
