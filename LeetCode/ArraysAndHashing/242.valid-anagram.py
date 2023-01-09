ed29bbe5ab1b9fc0598b524ab34fdc4665dbfcc3
Harish <harish.rajkumar2022@vitstudent.ac.in>
2023-01-09 21:31:49 +0530

    Add LeetCode 125

--- LeetCode/ArraysAndHashing/242.valid-anagram.py
+++
@@ -1,7 +0,0 @@
-# https://leetcode.com/problems/valid-anagram
-# Time: O(nlogn)
-# Space: O(1)
-
-class Solution(object):
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
---
+++ LeetCode/Easy/125.ValidPalindrome/README.md
@@ -0,0 +1,8 @@
+# 125. Valid Palindrome
+
+## [Question](https://leetcode.com/problems/valid-palindrome/)
+
+## My Solution:
+* Time complexity: O(n)
+* Space complexity: 0(1)
+* Concept(s) used: Two pointers
